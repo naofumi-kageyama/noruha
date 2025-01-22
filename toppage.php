ce4efe86582bc824ca1e6791784044a82a15097a
kageyama <n.kageyama0101@gmail.com>
2025-01-22 11:48:40 +0900

    topの次回公演部分を最適化

--- toppage.php
+++ toppage.php
@@ -8,95 +8,68 @@ Template Name: top
 <div class="renewal">
     <main class="p-top">
         <?php /* ここから左側 */ ?>
-        <section class="p-top__section p-top__left p-top-next-section">
-            <div class="p-top__section-inner">
+        <section class="p-top__section p-top__left">
+            <div class="p-top__section-inner p-top-next-section">
                 <?php while (have_posts()) :
                     the_post();
                 ?>
-                    <div class="top-left-title">
-                        <p>
-                            <?php
-                                $values = cfs()->get('label');
-                                foreach ( $values as $key => $label ):
-                            ?>
-                                <?php echo $label; ?>
-                            <?php endforeach; ?>
-                        </p>
-                    </div>
-
-                    <div class="top-next-visual">
+                    <h2 class="p-top-next-section__heading top-left-title">
+                        <?php
+                            $values = cfs()->get('label');
+                            foreach ( $values as $key => $label ):
+                        ?>
+                            <?php echo $label; ?>
+                        <?php endforeach; ?>
+                    </h2>
+                    <div class="p-top-next-section__main-visual">
                         <a href="<?php echo get_page_link('392'); ?>"><img src="<?php echo $cfs->get('next-mainvisual'); ?>"></a>
                     </div>
-                    <div class="next-title-wrapper">
-                        <p class="next-title"><?php echo $cfs->get('title'); ?></p>
-                        <p class="next-subtitle"><?php echo $cfs->get('subtitle'); ?></p>
+                    <div class="p-top-next-section__title-area">
+                        <h3 class="p-top-next-section__title"><?php echo $cfs->get('title'); ?></h3>
+                        <?php if(!empty($cfs->get('subtitle'))) : ?>
+                            <p class="p-top-next-section__subtitle"><?php echo $cfs->get('subtitle'); ?></p>
+                        <?php endif; ?>
                     </div>
-
-            <div class="top-next-wrapper">
-                <div class="top-next-detail">
-                    <div class="top-next-date-wrapper">
-                        <p class="top-next-detail-title">会期</p>
-                        <div class="top-next-detail-contents">
-                            <p><?php echo $cfs->get('date'); ?></p>
-                        </div>
-                    </div>
-                    <div class="top-next-venue-wrapper">
-                        <p class="top-next-detail-title">会場</p>
-                        <div class="top-next-detail-contents">
-                            <p class="top-next-venue-name"><?php echo $cfs->get('venue'); ?></p>
-                            <p>
-                                <?php echo $cfs->get('venue-detail'); ?><br>
-                                <a href="<?php $venueUrl = $cfs->get('venue-url'); echo esc_url($venueUrl); ?>" target="blank"><?php $venueUrl = $cfs->get('venue-url'); echo esc_url($venueUrl); ?></a>
-                            </p>
-                        </div>
-                    </div>
-                    <?php
-                        $fields = cfs()->get('people');
-                        if(!empty($fields)) :
-                    ?>
-                        <div class="top-next-people-wrapper">
-                            <p class="top-next-detail-title">人々</p>
-                            <div class="top-next-detail-contents">
-                                <?php
-                                    foreach ($fields as $field) :
-                                ?>
-                                    <div class="top-next-people">
-                                        <div class="top-next-role">
-                                            <p><?php echo $field['role']; ?></p>
-                                        </div>
-                                        <div class="top-next-name">
-                                            <p>
-                                                <?php echo $field['name']; ?>
-                                            </p>
-                                        </div>
-                                    </div>
-                                <?php endforeach; ?>
+                    <div class="p-top-next-section__description-area">
+                        <div class="p-top-next-section__description-section">
+                            <h4 class="p-top-next-section__description-heading">会期</h4>
+                            <div class="p-top-next-section__description-text-area">
+                                <p class="p-top-next-section__date"><?php echo $cfs->get('date'); ?></p>
                             </div>
                         </div>
-                    <?php endif ; ?>
-                </div>
-
-                        <div class="top-next-copy hide">
-                            <p>地下室に指示を待つ殺し屋が二人。傍には料理昇降機（ダム・ウェイター）。 不条理演劇の金字塔に《円盤に乗る派》が挑む！</p>
+                        <div class="p-top-next-section__description-section">
+                        <h4 class="p-top-next-section__description-heading">会場</h4>
+                            <div class="p-top-next-section__description-text-area">
+                                <p class="p-top-next-section__venue-name"><?php echo $cfs->get('venue'); ?></p>
+                                <p class="p-top-next-section__venue-detail"><?php echo $cfs->get('venue-detail'); ?></p>
+                                <p class="p-top-next-section__venue-url"><a href="<?php $venueUrl = $cfs->get('venue-url'); echo esc_url($venueUrl); ?>" target="blank"><?php $venueUrl = $cfs->get('venue-url'); echo esc_url($venueUrl); ?></a></p>
+                            </div>
                         </div>
-                        <div class="top-next-description hide">
-                            <p>
-                                「まるで、海の向こうの国から届くラジオの電波みたいに、不安定なまま、振動していた。それがおれの命だった。」<br>
-                                円盤に乗る派の新作公演『仮想的な失調』は、二つの古典作品を下敷きにした物語です。ひとつは、自分の名前すら忘れてしまう坊主を主人公とした狂言「名取川」。もうひとつは、源義経の西国落ちを題材にとり、涙の別れをする静御前と、かつて義経に滅ぼされた平家の亡霊を一人二役で演じる能「船弁慶」。常に複数のSNSを使い分け、様々なアイデンティティを駆使する現代の生活に向けて、これらの物語の新たな語り直しを試みます。そこから幽霊のように立ち上がってくるのは、私たちが根本的に抱えている根拠のない不安や、どこか既視感のある不気味さ。その先に、顔の見えないまま欲望が作用し合い、そのもつれが原因不明の"失調"を引き起こす現代社会のシステムを見つめ直すこともできるかもしれません。演出には、2020年の『おはようクラブ』でもコラボレーションした蜂巣ももを再び迎え、カゲヤマ気象台との新たな共同体制で挑みます。
-                            </p>
-                        </div>
+                        <?php
+                            $fields = cfs()->get('people');
+                            if(!empty($fields)) :
+                        ?>
+                            <div class="p-top-next-section__description-section">
+                            <h4 class="p-top-next-section__description-heading">人々</h4>
+                                <div class="p-top-next-section__description-text-area">
+                                    <?php foreach ($fields as $field) : ?>
+                                        <dl class="p-top-next-section__people-list">
+                                            <dt class="p-top-next-section__people-term"><?php echo $field['role']; ?></dt>
+                                            <dd class="p-top-next-section__people-description"><?php echo $field['name']; ?></dd>
+                                        </dl>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                        <?php endif ; ?>
                     </div>
-
-                    <div class="top-next-button">
-                        <a href="<?php echo get_page_link('392'); ?>">詳細</a>
-                    </div>
+                    <a class="p-top-next-section__button" href="<?php echo get_page_link('392'); ?>">詳細</a>
                 <?php endwhile; ?>
             </div>
         </section>
 
         <?php /* ここから右側 */ ?>
-        <section class="p-top__section p-top__right p-top-main-section">
-            <div class="p-top__section-inner">
+        <section class="p-top__section p-top__right">
+            <div class="p-top__section-inner p-top-main-section">
 
                 <?php include('menu.php'); ?>
 
