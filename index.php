3420c4e5c3289c7d3eda499d08f3734df85f0d34
kageyama <n.kageyama0101@gmail.com>
2025-01-29 11:51:02 +0900

    index.phpを最適化

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
 <?php get_header(); ?>
-
-<section class="global-wrapper">
-    <?php get_template_part('template-parts/nav'); ?>
-
-    <div class="contents-wrapper">
-        <?php if (have_posts()): ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-                <h1><?php echo get_the_title(); ?></h1>
-
-                <article class="basicContent">
-                    <?php the_content(); ?>
-                </article>
-                
-            </div>
-        <?php endwhile; ?>
-        <?php else: ?>
-        <!-- 投稿が無い場合の処理 -->
-        <?php endif; ?>
-    </div>
-</section>
+<main class="l-main">
+    <?php if (have_posts()): ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <article class="c-content">
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        </article>
+    <?php endwhile; ?>
+    <?php else: ?>
+        <p>投稿がありません。</p>
+    <?php endif; ?>
+</main>
 <?php get_footer(); ?>
