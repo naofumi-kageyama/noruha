ee172382ec19d5fc862e7342fd8f851284a06a1c
kageyama <n.kageyama0101@gmail.com>
2025-01-30 14:12:54 +0900

    contact-formのショートコードを修正

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="l-footer__form-container js-form-container">
             <button class="l-footer__close-button js-form-close"></button>
             <?php
-                echo do_shortcode('[contact-form-7 id="25" title="コンタクトフォーム 1" html_class="l-footer__form"]');
+                echo do_shortcode('[contact-form-7 id="d704324" title="メルマガ登録" html_class="l-footer__form"]');
             ?>
         </div>
     <?php endif; ?>
