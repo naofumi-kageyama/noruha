5ae39311da0dcfeae2f4123386d74539ed40d058
kageyama <n.kageyama0101@gmail.com>
2025-02-26 11:48:33 +0900

    index.phpでサムネイルが表示されるようになった

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
     <?php if (have_posts()): ?>
     <?php while (have_posts()) : the_post(); ?>
         <article class="c-content">
+            <?php if(has_post_thumbnail()) : ?>
+                <div class="c-content__thumbnail">
+                    <?php the_post_thumbnail('full'); ?>
+                </div>
+            <?php endif; ?>
             <h1><?php the_title(); ?></h1>
             <?php the_content(); ?>
         </article>
